7af2b49feb57be2d67466a4b96aac140c35ab404
zulfidly <zulfidly@gmail.com>
2023-07-15 19:36:42 +0800

    7

--- server/api/gettable.get.js
+++ server/api/gettable.get.js
@@ -16,18 +16,15 @@ export async function readTable1(years) {
             if(err) reject({statusCode: 500, "erroR": JSON.stringify(err)})
             else {
                 let temp = []
-                if(records.toString().includes('DOCTYPE')) reject('request rejected')
-                else {
                     records.forEach(function(record, ind) {         
                         // console.log(record.get('year'), typeof record.get('year'));           
-                        if(years.includes(record.get('year').trim())) {
-                            
+                        if(years.includes(record.get('year').trim())) {                            
                             temp.push({ year: record._rawJson.fields.year, datA: record._rawJson.fields.jsoN || '' })
                             // temp.push({ year: record.get('year'), datA: record._rawJson.fields.jsoN || '' })
                         } 
                     });
-                }
-                resolve(temp)
+                if(temp.includes('DOCTYPE')) reject('request rejected')
+                else resolve(temp)
             }
         });
     })
